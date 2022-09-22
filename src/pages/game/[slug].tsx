a16d8556975e9a651f29454a7b09ef3c86cb930c
John Greidinger <jp@greidinger.dev>
2022-09-22 20:14:04 -0300

    ux fix

--- src/pages/game/[slug].tsx
+++ src/pages/game/[slug].tsx
@@ -10,32 +10,24 @@ import { adsRouter } from "../../server/trpc/router/ads";
 import superjson from "superjson";
 import AdCard from "../../components/game/AdCard";
 import CreateAdModal from "../../components/home/CreateAdModal";
-import { AdQueryOutput } from "../../types/ads";
-import { ArrowsClockwise } from "phosphor-react";
-import { useRouter } from "next/router";
+import { trpc } from "../../utils/trpc";
+import Spinner from "../../components/Spinner";
 
-const GamePage = ({ ads }: InferGetStaticPropsType<typeof getStaticProps>) => {
-  const router = useRouter();
+const GamePage = ({ slug }: InferGetStaticPropsType<typeof getStaticProps>) => {
+  const { data: ads, isLoading } = trpc.ads.getAdsByGameSlug.useQuery({ slug });
   return (
     <div className="container flex items-center justify-center mx-auto h-screen">
       <ul className="flex gap-4 flex-wrap justify-center">
-        {ads && ads.length > 0 ? (
+        {isLoading ? (
+          <Spinner size={48} />
+        ) : ads && ads.length > 0 ? (
           ads.map((ad) => <AdCard key={ad.id} ad={ad} />)
         ) : (
           <div className="flex flex-col gap-4 px-8">
             <h1 className="font-bold text-2xl text-center">
               Não há anúncios para este jogo no momento
             </h1>
-            <div className="flex gap-2 justify-center">
-              <CreateAdModal />
-              <button
-                onClick={() => router.reload()}
-                className="btn btn-accent gap-1"
-              >
-                Atualizar
-                <ArrowsClockwise size={24} />
-              </button>
-            </div>
+            <CreateAdModal />
           </div>
         )}
       </ul>
@@ -57,7 +49,6 @@ export const getStaticPaths: GetStaticPaths = async () => {
 };
 
 export const getStaticProps: GetStaticProps<{
-  ads: AdQueryOutput;
   slug: string;
 }> = async (ctx: GetStaticPropsContext) => {
   const slug = ctx.params?.slug as string;
@@ -67,11 +58,11 @@ export const getStaticProps: GetStaticProps<{
     transformer: superjson,
   });
 
-  const ads = await ssg.getAdsByGameSlug.fetch({ slug });
+  await ssg.getAdsByGameSlug.fetch({ slug });
 
   return {
     props: {
-      ads,
+      trpcState: ssg.dehydrate(),
       slug,
     },
   };
